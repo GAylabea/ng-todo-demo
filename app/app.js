b9b825387ac55b35d36a6e8c8acb0bdc97a997d2
GAylabea <gbea@rocketmail.com>
2016-05-18 21:18:33 -0500

    navbar added with links

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
-console.log("hello"); 
+// we are adding the angular app - and in the parens, are the name of the app and empty brackets
+var app = angular.module("TodoApp", []); 
+
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name: "Logout"}, {name: "All Item"}, {name: "New Item"}]
+})
+
+// now a controller - we put the name of the controller (traditionally with Ctrl) and add a function with scope 
+  // don't forget to pu the name of the controller inside the index file
+app.controller("TodoCtrl", function($scope) {
+  $scope.welcome = "hello"; 
+
+})
