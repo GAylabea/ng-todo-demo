8ba7b41bf395888438de859d3eecf8e1a1e134b9
GAylabea <gbea@rocketmail.com>
2016-05-21 10:39:10 -0500

    set up form, made changes to controller function, everything works

--- app/app.js
+++ app/app.js
@@ -8,6 +8,50 @@ app.controller("NavCtrl", function($scope){
 // now a controller - we put the name of the controller (traditionally with Ctrl) and add a function with scope 
   // don't forget to pu the name of the controller inside the index file
 app.controller("TodoCtrl", function($scope) {
-  $scope.welcome = "hello"; 
+  $scope.welcome = "hello";
+  $scope.showListView = false;
+  $scope.newTask = {} ; 
+
+  $scope.items = [
+  {
+    id: 0,
+    task: "mow the lawn",
+    isCompleted: true,
+    dueDate: "12/5/17",
+    assignedTo: "greg",
+    location: "Zoe's house",
+    urgency: "low",
+    dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+  },
+  {
+    id: 1,
+    task: "grade quizzes",
+    isCompleted: false,
+    dueDate: "12/5/17",
+    assignedTo: "Joe",
+    location: "NSS",
+    urgency: "high",
+    dependencies: ["wifi", "tissue", "vodka"]
+  },
+  {
+    id: 2,
+    task: "take a nap",
+    isCompleted: false,
+    dueDate: "12/5/17",
+    assignedTo: "Zoe",
+    location: "Zoe's house",
+    urgency: "medium",
+    dependencies: ["hammock", "cat", "hat", "pillow", "blankey"]
+  }
+  ]
+
+  $scope.newItem = function(){
+    console.log("you clicked new item"); 
+    $scope.showListView = false;
+  }
+  $scope.allItem = function(){
+    console.log("you clicked all item");
+    $scope.showListView = true;
+  }
 
 })
