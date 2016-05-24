03668d06d81cdb31947ac0b9f09d675e0f36efc8
GAylabea <gbea@rocketmail.com>
2016-05-23 19:19:05 -0500

    changes to controllers and use of Firebase

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,8 +1,19 @@
 app.controller("ItemListCtrl", function($scope, $http) {
     $scope.items = []; 
-    // this is a GET request - similar to Ajax - note that we are connecting stuff from the index file which is at the root - so we don't need two dots
-    $http.get("./data/items.json")
+    // this is a GET request - similar to Ajax - note that we are connecting stuff from the index file which is at the root - 
+    // so we don't need two dots to go all the way into another directory. 
+    // Also - we are using similar syntax - get post push; here we are using "get"
+    $http.get("https://todolistnss.firebaseio.com/items.json")
       .success(function(itemObject){
-        console.log("ItemObject", itemObject);
+        var itemCollection = itemObject;
+        console.log("ItemObject", itemCollection);
+        // we are using the object and keys method here. for everything in the loop, we call it "item" 
+        // as it goes thru the loop which is an array of ALL the keys of the object, and each key
+        Object.keys(itemCollection).forEach(function(key){
+          // this says find the itemCollection object and grab the key I want and set the ID to key - it's basically giving it a firebase ID
+          itemCollection[key].id=key;
+          // then we push up the item - we're pushing an object into an items array
+          $scope.items.push(itemCollection[key]);
+        })
       })
 });
--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,12 +1,40 @@
-app.controller("ItemNewCtrl", function($scope) {
-  $scope.newTask = {};
-  $scope.items = []
+// this defines our keys - in case it's left blank, 
+app.controller("ItemNewCtrl", function($scope, $http, $location) {
+  $scope.newTask = {
+    assigedTo: "",
+    dependencies: "",
+    dueDate: "",
+    isCompleted: false,
+    location: "",
+    task: "",
+    urgency: ""
+  };
 
+// we are creating parameters for the new object to load - we are pulling all the info from the form
 $scope.addNewItem = function() {
-  $scope.newTask.isCompleted = false; 
-  $scope.newTask.id = $scope.items.length; 
-  console.log("you added new item", $scope.newTask);
-  $scope.items.push($scope.newTask);  
-  $scope.newTask = ""; 
+  $http.post(
+    "https://todolistnss.firebaseio.com/items.json", 
+    JSON.stringify({
+      assigedTo: $scope.newTask.assigedTo,
+      dependencies: $scope.newTask.dependencies,
+      dueDate: $scope.newTask.dueDate,
+      isCompleted: false,
+      location: $scope.newTask.location,
+      task: $scope.newTask.task,
+      urgency: $scope.newTask.urgency
+    })
+    )
+  .success(function(response) {
+    console.log(response); 
+    // this $location allows us to change the URL window - we could have used any other view - details, new, etc. 
+    // - it needs to be directed to something that is actually in your app
+    $location.url("/items/list")
+  })
+
+  // $scope.newTask.isCompleted = false; - this is now in the object
+  // $scope.newTask.id = $scope.items.length; - this line kept track of our objects but firebase does that now so...
+  // console.log("you added new item", $scope.newTask); 
+  // $scope.items.push($scope.newTask); - this was pushing to the old array but we changed it to objects 
+  // $scope.newTask = ""; - this was saving to a new string
   }
 });
